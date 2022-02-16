10a7313ae1416482b0c8a68f2f2b41a6d2b701eb
Mysik <Mishamysik@gmail.com>
2022-02-16 14:16:47 +0200

    whatgpt3 block create

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './whatGPT3.css';
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
     return (
-        <div>WhatGPT3</div>
+        <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+            <div className="gpt3__whatgpt3-feature">
+                <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">
+                    The possibilities are beyond your imagination
+                </h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " title="Chatbots"/>
+                <Feature text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments. " title="Knowledgebase"/>
+                <Feature text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments. " title="Education"/>
+            </div>
+        </div>
     )
 }
 
